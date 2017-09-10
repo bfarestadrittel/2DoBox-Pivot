98d668db1c59aba22451df52bffaba941de7f7c0
Brandon Farestad-Rittel <bfarestadrittel@gmail.com>
2017-09-10 14:28:53 -0600

    cleaned up naming conventions

--- script/script.js
+++ script/script.js
@@ -1,49 +1,50 @@
 //listeners for enabling save button
-$('.idea-input').keyup(enabledBtn);
-$('.main-title').keyup(enabledBtn);
+$('.body-input').keyup(enabledBtn);
+$('.title-input').keyup(enabledBtn);
 
 
 //on page load retrieve local storage
-$(document).ready(function() {
-getIdea();
-});
+// $(document).ready(function() {
+// getIdea();
+// });
 
-//retrieve parsed local storage or empty array
-function getIdea (id) {
-	var retrieveIdea = JSON.parse(localStorage.getItem(id));
-	if (retrieveIdea ) {
-		return retrieveIdea;
-	} else {
-		return [];
-	}
-};
+// //retrieve parsed local storage or empty array
+// function getIdea (id) {
+// 	var retrieveIdea = JSON.parse(localStorage.getItem(id));
+// 	if (retrieveIdea ) {
+// 		return retrieveIdea;
+// 	} else {
+// 		return [];
+// 	}
+// };
 
 
 //enabling button on both fields keyup
 function enabledBtn() {
-    if ( $('.idea-input').val() === "" && $('.main-title').val() === "") {
-      $('#submit-button').attr("disabled", true)
+    if ( $('.body-input').val() === "" && $('.title-input').val() === "") {
+      $('.submit-button').attr("disabled", true)
     } else {
-      $('#submit-button').attr("disabled", false);
+      $('.submit-button').attr("disabled", false);
     }
 };
 
 //call addIdea function on click of submit button
-$('#submit-button').on('click', addIdea);
+$('.submit-button').on('click', addIdea);
 
 //on keyup of title and body inputs
-$('.main-title', '.idea-input').on('keyup', function (e) {
+$('.title-input', '.body-input').on('keyup', function (e) {
 	//if enter button (keyCode13) is pressed and both inputs have a value
-	if (e.keyCode === 13 && ($('.main-title').val() && $('.idea-input').val())){
+	if (e.keyCode === 13 && ($('.title-input').val() && $('.body-input').val())){
 		addIdea(e)
 	}
 });
 
-//////////////////////////////////////global variable .... array??
+//////////////////////////////////////refactor into a function we can recall throughout
 var keys = Object.keys(localStorage)
 
 keys.forEach(function(key){
-localStorage[key]
+	// console.log(key);
+// localStorage[key]
 prependIdea(JSON.parse(localStorage[key]))
 })
 ///////////////////////////////////////////////
@@ -61,8 +62,8 @@ function Idea (title, body, status ) {
 //taking input values, creating idea object, prepending, sending storage
 function addIdea (e) {
 	e.preventDefault();
-	var title = $('.main-title').val();
-	var body = $('.idea-input').val();
+	var title = $('.title-input').val();
+	var body = $('.body-input').val();
 	///do you need this??
 	var status = 'swill';
 	//new object called anotherIdea
@@ -73,29 +74,30 @@ function addIdea (e) {
 
 //prepending created ideacards, using new stored values, clearing input fields
 function prependIdea (idea) {
-	$('.bookmark-list').prepend(
-		`<article id=${idea.id} class="idea-article">
-		<h2 class="idea-title" contenteditable=true >${idea.title}</h2> 
-		<div class="delete-button-div icon-buttons delete-button right">
+	$('.card-list').prepend(
+		`<article id=${idea.id} class="card">
+		<h2 class="card-title" contenteditable="true">${idea.title}</h2> 
+		<div class="delete-button-div icon-buttons delete-button">
 		</div>
-		<p contenteditable="true" class="idea-paragraph">${idea.body}</p>
-		<div class="quality-rank"> 
+		<p contenteditable="true" class="card-body">${idea.body}</p>
+		<div class="rank"> 
 		<div class="upvote-button-div icon-buttons upvote-button">
 		</div>
 		<div class="downvote-button-div icon-buttons downvote-button"> 
 		</div>
-		<p> quality: <span class = "quality-content">${idea.status}</span> </p> </div>
+		<p> quality: <span class="rank-status">${idea.status}</span> </p> </div>
 		<hr /> 
 		</article>`)
-	$('.main-title').val("");
-	$('.idea-input').val("");
+	$('.title-input').val("");
+	$('.body-input').val("");
+	enabledBtn()
 }
 
 //eventlistener for delete button and function to remove from section and localStorage
-$('.bookmark-list').on('click', '.delete-button-div', function() {
-	$(this).closest('.idea-article').remove();
+$('.card-list').on('click', '.delete-button-div', function() {
+	$(this).closest('.card').remove();
 	//maybe just update local storage here rather than removeItem
-	localStorage.removeItem(($(this).closest('.idea-article').attr('id')));
+	localStorage.removeItem(($(this).closest('.card').attr('id')));
 });
 
 //sending to localStorage (as above by passing in id and newly created card)
@@ -105,42 +107,42 @@ function storeIdea (id, card) {
 
 //event listener on section for upvote - function if quality is swill - change to plausible, else change to genius
 //and send to localStorage with new quality string
-$('.bookmark-list').on('click', '.upvote-button-div', function() {
-	 	var checkStatus = $(this).closest('.quality-rank').find('.quality-content').text(); //grab the quality content span (swill)
-	 	var id = ($(this).closest('.idea-article').attr('id'));
+$('.card-list').on('click', '.upvote-button-div', function() {
+	 	var checkStatus = $(this).closest('.rank').find('.rank-status').text(); //grab the quality content span (swill)
+	 	var id = ($(this).closest('.card').attr('id'));
 	 	var uniqueCard = JSON.parse(localStorage.getItem(id));
 	 	if (checkStatus === 'swill') {
-	     	$(this).closest('.quality-rank').find('.quality-content').text('plausible');
+	     	$(this).closest('.rank').find('.rank-status').text('plausible');
 	     	uniqueCard.status = 'plausible';
 			localStorage.setItem(id, JSON.stringify(uniqueCard));
      	} else {
-     		$(this).closest('.quality-rank').find('.quality-content').text('genius');
+     		$(this).closest('.rank').find('.rank-status').text('genius');
      		uniqueCard.status = 'genius';
      		localStorage.setItem(id, JSON.stringify(uniqueCard));
      	}
 	});
 
 // same as upvote but for downvoting
-$('.bookmark-list').on('click', '.downvote-button-div', function() {
-  	var checkStatus = $(this).closest('.quality-rank').find('.quality-content').text();
-  	var id = ($(this).closest('.idea-article').attr('id'));
+$('.card-list').on('click', '.downvote-button-div', function() {
+  	var checkStatus = $(this).closest('.rank').find('.rank-status').text();
+  	var id = ($(this).closest('.card').attr('id'));
 	var uniqueCard = JSON.parse(localStorage.getItem(id));
   if (checkStatus === 'genius') {
-		$(this).closest('.quality-rank').find('.quality-content').text('plausible');
+		$(this).closest('.rank').find('.rank-status').text('plausible');
 		uniqueCard.status = 'plausible';
 		localStorage.setItem(id, JSON.stringify(uniqueCard));
   } else {
-  		$(this).closest('.quality-rank').find('.quality-content').text('swill');
+  		$(this).closest('.rank').find('.rank-status').text('swill');
   		uniqueCard.status = 'swill';
 		localStorage.setItem(id, JSON.stringify(uniqueCard));
   }
 });
 
 //on keyup of body content(after edit) sent to localStorage 
-$('.bookmark-list').on('keyup', '.idea-paragraph', editBody);
+$('.card-list').on('keyup', '.card-body', editBody);
 //also makes enterkey focus out
 function editBody(e) {
-var id = ($(this).closest('.idea-article').attr('id'));
+var id = ($(this).closest('.card').attr('id'));
 var uniqueCard = JSON.parse(localStorage.getItem(id));
 if (e.keyCode === 13) {
 	e.preventDefault();
@@ -151,10 +153,10 @@ localStorage.setItem(id, JSON.stringify(uniqueCard));
 }
 
 //on keyup of title content... ^^^^
-$('.bookmark-list').on('keyup', '.idea-title', editTitle);
+$('.card-list').on('keyup', '.card-title', editTitle);
 
 function editTitle (event){
-var id = ($(this).closest('.idea-article').attr('id'));
+var id = ($(this).closest('.card').attr('id'));
 var uniqueCard = JSON.parse(localStorage.getItem(id));
 if (event.keyCode === 13) {
 	event.preventDefault();
@@ -168,10 +170,10 @@ localStorage.setItem(id, JSON.stringify(uniqueCard));
 ///////////NEED HELP UNDERSTANDING////////////////////////////////////////
 //grab search value and run doYouMatch function to add or remove class to display
 function realtimeSearch() {
-    var searchTerm = $('.search-box').val().toUpperCase();
+    var searchTerm = $('.search-input').val().toUpperCase();
     // console.log(searchTerm);
-    $('.idea-article').each ( function (index, element) {
-		// console.log(element);
+    $('.card').each ( function (index, element) {
+		// console.log(index + element);
 	if (doYouMatch(searchTerm, index)) {
             $(element).removeClass('card-display-none');
         } else {
@@ -181,16 +183,16 @@ function realtimeSearch() {
 };
 
 //event listener for keyup on search input
-$('.search-box').on('keyup', realtimeSearch)
+$('.search-input').on('keyup', realtimeSearch)
 
 //why? is ideatitle/paragraph an array? why do we look for the index? 
 //how is the !== -1 thing working?????
 function doYouMatch (searchTerm, index) {
-	var title = $($('.idea-title')[index]).html();
+	var title = $($('.card-title')[index]).html();
 	var upperCaseTitle = title.toUpperCase();
-	var body = $($('.idea-paragraph')[index]).html();
+	var body = $($('.card-body')[index]).html();
 	var upperCaseBody = body.toUpperCase();
-
+	console.log(index)
 	// console.log(title)
 	// console.log(body)
 
